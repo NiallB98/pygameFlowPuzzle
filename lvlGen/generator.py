9d08912197558790ebf0bb220b24d0c77f7f9dcd
Niall <44234089+NiallB98@users.noreply.github.com>
2021-01-09 16:48:07 +0000

    Update generator.py

--- lvlGen/generator.py
+++ lvlGen/generator.py
@@ -1,59 +1,6 @@
 import os
 import numpy as np
 
-"""
-[PLANNING]
-
-
-    Rules:
-        
-    - Weigh where to start new line
-    - Dont let line extend again in the same direction unless it cant go in any other direction
-    - If a line has more length to use but cant extend any more give more length to a new line
-    - Every line but last cannot create a 1-2 block empty space
-    - Randomly set lengths of lines then the last one is given a number to fill the rest
-    - When creating a line, ask it how many blocks it extends across
-    - Give lines a random max length to extend for a certain number of steps
-    - The new addition to the line cannot have >1 blocks of the same colour next to it
-    - Empty boxes keep track of the number of empty boxes beside them
-    - If adjEmpty == 1 then place the start of a new line
-    - After all possible lines have been made and there is still empty space the program redoes lines
-    - Any 2x2 grid from inside the overall grid cannot be the same colour
-
-[PSEUDO CODE]
-
-numColours = %
-n = 9
-grid = numpy.zeros(shape=(n + 2, n + 2)) - 1
-for i in range(n + 2):
-    for j in range(n + 2):
-        if (i == ) or (j == -0) or (i == n + 1) or (j == n + 1):
-            grid[i, j] = -2
-            
-print(grid)
-
-numSq = gridSize^2
-
-nLeft = numColours
-
-while numSq > 0:
-    ###(assigning line length randomly based on what length is left)###
-    if numSq - (nLeft - 1) * 3 != 3: lineLength = random.randint(3, numSq - (nLeft - 1) * 3)
-    else: lineLength = 3
-    
-    ###(placing start point randomly in a place that fits)###
-    if 
-    
-    ###(snaking along grid till length has run out)###
-    
-    
-    ###(Updating numSq and number of lines left)###
-    numSq -= lineLength
-    nLeft--
-
-"""
-#######
-
 def numColourBeside(i, j, grid, colour=-1): # Returns Number of Squares That Are Empty Around Current
     count = 0
     if grid[i, j - 1] == colour: count += 1 # ABOVE
